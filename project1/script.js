6dff3b5d95cc2cafffdb54b84d368f5e8cd4a9f9
chaitanyakwatra <136676697+chaitanyakwatra@users.noreply.github.com>
2023-07-05 15:49:55 +0530

    tried to use slick from line 94 to 158

--- project1/script.js
+++ project1/script.js
@@ -1,21 +1,8 @@
 $(document).ready(function(){
-    $('.fashion__cloths').slick({
+    $('.slides').slick({
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 3
     });
   });
-  $(document).ready(function(){
-    $('.Electronics__item').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    });
-  });
-  $(document).ready(function(){
-    $('.accessories__Jewellery').slick({
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    });
-  });
+  
