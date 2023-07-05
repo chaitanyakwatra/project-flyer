407295d2682882059a3db3bcf3e5dc97678960da
chaitanyakwatra <136676697+chaitanyakwatra@users.noreply.github.com>
2023-07-05 13:23:39 +0530

    spacing elements, header, slick slider try
    slick slider is showing an error or is not working in most situations. i have tried a few methods but there is no changes.

---
+++ project1/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.fashion__cloths').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+  });
+  $(document).ready(function(){
+    $('.Electronics__item').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+  });
+  $(document).ready(function(){
+    $('.accessories__Jewellery').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+  });
